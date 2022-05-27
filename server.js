8bb5cd06932b2122ddd99eaa38d26354f069370e
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-05-27 12:45:50 -0500

    Fixed file locations

--- server.js
+++ server.js
@@ -1,9 +1,54 @@
+const fs = require('fs');
 const express = require('express');
+const path = require('path');
+var uniqid = require('uniqid'); 
+
 const app = express();
 const port = 3001;
 
-app.get('/', (req,res) => {
-    res.send('test')
+app.use(express.json());
+app.use(express.urlencoded( {extended:true} ));
+app.use(express.static('public'));
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname,'./develop/public/notes.html'))
 })
 
-app.listen(process.env.PORT || port, () => console.log('App listening at https://localhost:${port}'));
+app.get('/api/notes', (req,res) => {
+    fs.readFile('./develop/db/db.json', (error,data) => {
+        if(error) { 
+            throw error;
+        }
+        res.sendFile(path.join(__dirname,'./develop/db/db.json'))
+    })
+})
+
+app.post('/api/notes', (req,res) => {
+    let newNote = req.body;
+    newNote.id = uniqid();
+    console.log(newNote);
+    
+    fs.readFile('./develop/db/db.json', (error,data) => {
+        let parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        let newDB = JSON.stringify(parsedNotes);
+        fs.writeFile('./db/db.json', newDB, error => {
+            if(error) {
+                throw error;
+            }
+            console.log(newDB)
+            console.log('New data added')
+        })
+    });  
+})
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'./develop/public/index.html'))
+})
+
+app.listen(process.env.PORT || port, () => console.log(`App listening at https://localhost:${port}`));
+
+// const PORT = 3001;
+
+// app.listen(PORT, () => console.log(`App listening at http://localhost:${port}`));
