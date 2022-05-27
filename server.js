0469256479d89165b9557f41cca5e04a01d70664
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-05-27 12:58:46 -0500

    Simplified file structure

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(express.urlencoded( {extended:true} ));
 app.use(express.static('public'));
 
 app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname,'./develop/public/notes.html'))
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
 })
 
 app.get('/api/notes', (req,res) => {
-    fs.readFile('./develop/db/db.json', (error,data) => {
+    fs.readFile('./db/db.json', (error,data) => {
         if(error) { 
             throw error;
         }
-        res.sendFile(path.join(__dirname,'./develop/db/db.json'))
+        res.sendFile(path.join(__dirname,'./db/db.json'))
     })
 })
 
@@ -28,7 +28,7 @@ app.post('/api/notes', (req,res) => {
     newNote.id = uniqid();
     console.log(newNote);
     
-    fs.readFile('./develop/db/db.json', (error,data) => {
+    fs.readFile('./db/db.json', (error,data) => {
         let parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
 
@@ -44,11 +44,11 @@ app.post('/api/notes', (req,res) => {
 })
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname,'./develop/public/assets/index.html'))
+    res.sendFile(path.join(__dirname,'./public/index.html'))
 })
 
-app.listen(process.env.PORT || port, () => console.log(`App listening at https://localhost:${port}`));
+// app.listen(process.env.PORT || port, () => console.log(`App listening at https://localhost:${port}`));
 
-// const PORT = 3001;
+const PORT = 3001;
 
-// app.listen(PORT, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(PORT, () => console.log(`App listening at http://localhost:${port}`));
